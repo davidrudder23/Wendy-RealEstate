cd6ec298045e96154296ca00eb7e18f118060791
George Colon <gcolon021@gmail.com>
2020-10-08 18:26:27 -0400

    Updates

---
+++ precache-manifest.922e722be4cd8018f32b3087f7204d36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3480f6b97ac24a4f97b76e9a18ebbf65",
+    "url": "/Wendy-RealEstate/index.html"
+  },
+  {
+    "revision": "b1d0facb5b2ffac9257e",
+    "url": "/Wendy-RealEstate/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "c983476393403f09d3b0",
+    "url": "/Wendy-RealEstate/static/css/main.f8f5a137.chunk.css"
+  },
+  {
+    "revision": "b1d0facb5b2ffac9257e",
+    "url": "/Wendy-RealEstate/static/js/2.6c2fc181.chunk.js"
+  },
+  {
+    "revision": "f6a33b08b8dab27dd8ab525dc9994832",
+    "url": "/Wendy-RealEstate/static/js/2.6c2fc181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c983476393403f09d3b0",
+    "url": "/Wendy-RealEstate/static/js/main.bd5833c2.chunk.js"
+  },
+  {
+    "revision": "480cd12a36444b8793f3",
+    "url": "/Wendy-RealEstate/static/js/runtime-main.79e7d5d7.js"
+  }
+]);
