1201668e1d530fe6bd24c55a0c7ccb8d27495a57
gcolon021 <gcolon021@gmail.com>
2020-10-14 16:43:19 -0400

    Added "google-spreadsheet": "^3.0.13" to package.json Updated TODOs Migrated AdditionalInformation Page to useCustomFormHook Added LenderTest.js as a test page to work on google-spreadsheet

--- frontend/src/components/Forms/AdditionalInformation.js
+++ frontend/src/components/Forms/AdditionalInformation.js
@@ -1,25 +1,15 @@
 import React from 'react'
 import FormHeader from "../FormFields/FormHeader";
-import { useForm } from "react-hook-form";
-import { useHistory } from "react-router-dom";
-import { useStateMachine } from 'little-state-machine';
-import updateAction from '../../state/updateState';
 import * as S from "../FormFields/FormStyled";
 // import InputField from "../FormFields/InputField";
-import { yupResolver } from '@hookform/resolvers';
-import { AdditionalInformationValidation } from "../../validation";
+// import { yupResolver } from '@hookform/resolvers';
+// import { AdditionalInformationValidation } from "../../validation";
 import { Next, Back } from "../FormFields/SharedButtons";
 import { handleDeploymentPath } from "../../shared";
+import useCustomFormHook from "../../hooks/useCustomFormHook";
 
 const AdditionalInformation = () => {
-    const { state, action } = useStateMachine(updateAction);
-    const { push } = useHistory();
-    const { handleSubmit } = useForm({
-        defaultValues: state.details,
-        mode: 'onChange',
-        reValidateMode: 'onChange',
-    });
-
+    const { handleSubmit, action, push } = useCustomFormHook();
     const onSubmit = data => {
         action(data);
         if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_ENABLE_REDIRECT === "false") {
---
+++ frontend/src/components/Forms/LenderTest.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useCustomFormHook from "../../hooks/useCustomFormHook";
+
+const LenderTest = () => {
+    const { register, handleSubmit, errors, action, push, getValues } = useCustomFormHook();
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LenderTest
