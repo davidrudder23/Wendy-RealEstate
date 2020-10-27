b3b369cb281c9a1f2c2cf2ea0a020c64ee3f005c
George Colon <gcolon021@gmail.com>
2020-10-26 20:18:25 -0400

    Updated Tenant
    - Tenant now has method that will load fields based
    on count.

--- frontend/src/components/Forms/Tenant.js
+++ frontend/src/components/Forms/Tenant.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as S from "../FormFields/FormStyled";
+import InputField from '../FormFields/InputField';
 
-const tenant = () => {
+const Tenant = () => {
+    const [count, setCount] = useState(3)
+
+    const increaseCount = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        if (count < MAX_BUYERS) {
+            setCount(count => count + 1);
+        }
+    }
+
+    const decreaseCount = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        setCount(count => count - 1);
+    }
+
+    const renderTenants = () => {
+        let tenants = [];
+        for (let index = 0; index < count; index++) {
+            tenant.push(
+                <S.MultiContainer key={index}>
+                    <InputField
+                        name={`client.${title}.${i}.firstName`}
+                        label="First Name"
+                        errors={errors?.client?.[title]?.[i]?.firstName}
+                        register={register}
+                        required={true}
+                        getValues={getValues} />
+                    <CustomDatePicker
+                        getValues={getValues}
+                        showYearPicker={true}
+                        control={control}
+                        name="property.dateHouseBuilt"
+                        label="Select Date Built"
+                        required={true}
+                        dateFormat="yyyy/MM"
+                    />
+                </S.MultiContainer>
+            )
+        }
+
+        return tenants;
+    }
+
     return (
-        <div>
+        <S.FieldWrapper>
 
-        </div>
+        </S.FieldWrapper>
     )
 }
 
-export default tenant
+export default Tenant
--- frontend/src/components/Forms/Testing.js
+++ frontend/src/components/Forms/Testing.js
@@ -5,9 +5,6 @@ import { Next, Back } from "../FormFields/SharedButtons";
 import useCustomFormHook from '../../hooks/useCustomFormHook';
 import { ClientValidation } from "../../validation";
 import { handleDeploymentPath } from "../../shared";
-import AutoComplete from '../FormFields/AutoComplete';
-import Address from '../FormFields/Address';
-import InputField from "../FormFields/InputField"
 
 const Testing = () => {
     const { handleSubmit, action, push, getValues, errors, register } = useCustomFormHook(ClientValidation);
@@ -16,42 +13,10 @@ const Testing = () => {
         push(handleDeploymentPath("/result"));
     }
 
-    const [controlData, setControlData] = React.useState("")
-
     return (
         <S.Container>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <FormHeader />
-                <S.FieldWrapper>
-                    <S.FieldTitle>Google Sheet AutoComplete Test</S.FieldTitle>
-                    <div>
-                        <AutoComplete
-                            label="control group"
-                            suggestions={["george", "kate", "luna", "finnick"]}
-                            useDefaultFilter={true}
-                            value={controlData}
-                            onChange={(e) => setControlData(e.currentTarget.value)}
-                        />
-                    </div>
-                    <S.MultiContainer>
-                        <InputField
-                        getValues={getValues}
-                        name="property.mlsNumber"
-                        label="MLS Number" 
-                        errors={errors["property"]?.mlsNumber}
-                        register={register} 
-                        required={true} />
-                        <S.AddressWrapper>
-                        <Address
-                        getValues={getValues}
-                        name="property.address"
-                        label="Address" 
-                        errors={errors["property"]?.address}
-                        register={register} 
-                        required={true} />
-                        </S.AddressWrapper>
-                    </S.MultiContainer>
-                </S.FieldWrapper>
                 <br />
                 <Back />
                 <Next />
