8f25c32a21d0e3fd79596a4e34b92317a987812b
gcolon021 <gcolon021@gmail.com>
2020-10-29 15:36:14 -0400

    Updates

---
+++ precache-manifest.c25f1d0f1b86a057b08b7a6de588204f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7958fc77f3607bb62dd412fc4f6e58",
+    "url": "/Wendy-RealEstate/index.html"
+  },
+  {
+    "revision": "d531a1dc46aab15b8a3d",
+    "url": "/Wendy-RealEstate/static/css/2.bac53e54.chunk.css"
+  },
+  {
+    "revision": "77b496d810c9a4718d21",
+    "url": "/Wendy-RealEstate/static/css/main.f8f5a137.chunk.css"
+  },
+  {
+    "revision": "d531a1dc46aab15b8a3d",
+    "url": "/Wendy-RealEstate/static/js/2.59917142.chunk.js"
+  },
+  {
+    "revision": "e267ae8e0bcc46a4ded0402fe6cf5aa2",
+    "url": "/Wendy-RealEstate/static/js/2.59917142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b496d810c9a4718d21",
+    "url": "/Wendy-RealEstate/static/js/main.ea864048.chunk.js"
+  },
+  {
+    "revision": "480cd12a36444b8793f3",
+    "url": "/Wendy-RealEstate/static/js/runtime-main.79e7d5d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Wendy-RealEstate/precache-manifest.9921047fe5bc47bdddd58a78f11f588e.js"
+  "/Wendy-RealEstate/precache-manifest.c25f1d0f1b86a057b08b7a6de588204f.js"
 );
 
 self.addEventListener('message', (event) => {
